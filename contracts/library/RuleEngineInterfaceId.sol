78e6e48e491e01e853e6490e061189875d4785ea
Ryan Sauge <71391932+rya-sge@users.noreply.github.com>
2026-02-09 12:13:30 +0100

    Update RuleEngine interfaceId

--- contracts/library/RuleEngineInterfaceId.sol
+++ contracts/library/RuleEngineInterfaceId.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.20;
 
 library RuleEngineInterfaceId {
-      bytes4 public constant RULE_ENGINE_INTERFACE_ID = 0x78a8de7d;
+      bytes4 public constant RULE_ENGINE_INTERFACE_ID = 0x20c49ce7;
 }
